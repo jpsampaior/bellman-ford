977f012fc5afd7f7c4eeac8547642dd501220525
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-06-05 15:52:14 -0300

    :sparkles: txt to matrix

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-input,# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from weight_matrix import WeightMatrix
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def read_data(filename):
+    data = []
+    with open(filename, "r") as file:
+        for line in file:
+            parts = line.strip().split()
+            data.append(parts)
+    return data
 
 
-# Press the green button in the gutter to run the script.
+def create_vertexes(number):
+    vertexes = []
+    for i in range(number):
+        vertexes.append(i + 1)
+    return vertexes
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    graph_input = read_data("input.txt")
+    num_vertexes = int(graph_input[0][0])
+    vertexes = create_vertexes(num_vertexes)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    edges = []
+    edges_weight = []
+
+    for edge_info in graph_input[1:]:
+        edge = [int(edge_info[0]), int(edge_info[1])]
+        weight = int(edge_info[2])
+        edges.append(edge)
+        edges_weight.append(weight)
+
+    graph = WeightMatrix(vertexes, edges, edges_weight).graph
+
+    for row in graph:
+        print(' '.join(map(str, row)))
